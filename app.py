1f652b075a79ee3b7b3dc49d856d38992686d036
Rohitkujur620 <87999637+Rohitkujur620@users.noreply.github.com>
2023-07-11 20:36:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from hashids import Hashids
 from flask import Flask, render_template, request, flash, redirect, url_for
 
 
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
